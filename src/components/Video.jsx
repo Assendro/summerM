79aed742b57543c0088ac9eb6094bcb78bf91de0
Assendro <kirilll999127@gmail.com>
2025-02-13 17:07:17 +0300

    заменил видеохолдеры на картинки

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,10 +5,6 @@ import './video.scss'
 
 const Video = ({ videoUrl, thumbnailVideoUrl }) => {
     const [isPlaying, setIsPlaying] = useState(false);
-    const videoStyle = {
-      margin: 'auto'
-    }
-
 
 
     return (
@@ -23,21 +19,14 @@ const Video = ({ videoUrl, thumbnailVideoUrl }) => {
             </div>
           {!isPlaying ? (
             // Миниатюра с предпросмотром (отдельное видео)
-            <ReactPlayer
-              url={thumbnailVideoUrl}
-              playing={true} // Миниатюра будет воспроизводиться автоматически
-              loop={true} // Зацикливание миниатюры
-              muted={true} // Без звука
-              width="400px"
-              height='auto'
-              style={videoStyle}
-              margin='auto'
+            <img
+              src={thumbnailVideoUrl}
+              className='placeholder'
               onClick={() => setIsPlaying(true)} // Начать воспроизведение основного видео
             />
           ) : (
             // Основное видео
             <ReactPlayer
-              style={videoStyle}
               width="400px"
               height='auto'
               url={videoUrl}

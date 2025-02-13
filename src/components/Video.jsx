118c75f05aee69c38379a38cb9ba435f034a3ebb
Assendro <kirilll999127@gmail.com>
2025-02-13 12:30:04 +0300

    исправил ошибки в указаниях ширины и длинны видео

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -12,31 +12,34 @@ const Video = ({ videoUrl, thumbnailVideoUrl }) => {
 
 
     return (
-        <div 
-            className='video'
-        >
-        {!isPlaying ? (
-          // Миниатюра с предпросмотром (отдельное видео)
-          <ReactPlayer
-            url={thumbnailVideoUrl}
-            playing={true} // Миниатюра будет воспроизводиться автоматически
-            loop={true} // Зацикливание миниатюры
-            muted={true} // Без звука
-            width="400px"
-            style={videoStyle}
-            margin='auto'
-            onClick={() => setIsPlaying(true)} // Начать воспроизведение основного видео
-          />
-        ) : (
-          // Основное видео
-          <ReactPlayer
-            style={videoStyle}
-            width="400px"
-            url={videoUrl}
-            playing={true}
-            controls={true}
-          />
-        )}
+      <div className="video-container">
+          <div 
+              className='video'
+          >
+          {!isPlaying ? (
+            // Миниатюра с предпросмотром (отдельное видео)
+            <ReactPlayer
+              url={thumbnailVideoUrl}
+              playing={true} // Миниатюра будет воспроизводиться автоматически
+              loop={true} // Зацикливание миниатюры
+              muted={true} // Без звука
+              width="400px"
+              height='auto'
+              style={videoStyle}
+              margin='auto'
+              onClick={() => setIsPlaying(true)} // Начать воспроизведение основного видео
+            />
+          ) : (
+            // Основное видео
+            <ReactPlayer
+              style={videoStyle}
+              width="400px"
+              url={videoUrl}
+              playing={true}
+              controls={true}
+            />
+          )}
+        </div>
       </div>
     )
 }

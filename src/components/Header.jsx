c21879bc5a88cc088477c7750fb319a25f716653
Assendro <kirilll999127@gmail.com>
2025-02-13 13:17:35 +0300

    заменил изображения на текст

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,11 @@ import './header.scss'
 const Header = () => {
     return (
         <div className="header-container">
-            <img className='text-img' src="https://x-lines.ru/letters/i/cyrillicscript/0041/e17819/40/0/4nppdygosdeadwf74n9pbpgosdeabwcb4n7pbqgozropbqstodembwf3.png" alt="" />
-            <br />
-            <img className='text-img' src="https://x-lines.ru/letters/i/cyrillicscript/0041/e17819/40/0/4ntpbpqozueabwcq4n7pdyqozmemtwf3rdeabwfo4nh7bxsozw.png" alt="" />
-            <br />
-            <img className='text-img' src="https://x-lines.ru/letters/i/cyrillicscript/0041/d7b337/40/0/4np7besouaonfwrh4n9pdyqozmemfwfore.png" alt="" />
-            <br />
-            <img className='text-img' src="https://x-lines.ru/letters/i/cyrillicscript/0086/d7b337/40/0/grhuopjpgrhu1cby4n37bc3q.png" alt="" />
+            Краснодарский край  <br />
+            Темрюкский район <br />
+            Винсовхоз "Прогресс" <br />
+            ЛТО "Москва" <br />
+            1985-1990 гг. 
         </div>
     )
 }

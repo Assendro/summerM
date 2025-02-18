c3b609cb676970d0d49c1d49f944863bb6b07e18
Assendro <kirilll999127@gmail.com>
2025-02-18 14:16:01 +0300

    добавили надписи к видео

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,25 +5,36 @@ import './videos.scss'
 const Videos = ({videoOpacity}) => {
     return (
         <div className="videos-container">
+
             <Video 
                 videoOpacity = {videoOpacity}
                 videoUrl={`${process.env.PUBLIC_URL}/videos/example1.mp4`}
                 thumbnailVideoUrl={`${process.env.PUBLIC_URL}/img/placeholder1.jpg`}
+                text={
+                    <>
+                        Утро в лагере
+                        <br />
+                        зарядка, работа
+                    </>
+                }
             />
             <Video 
                 videoOpacity = {videoOpacity}
                 videoUrl={`${process.env.PUBLIC_URL}/videos/example1.mp4`}
                 thumbnailVideoUrl={`${process.env.PUBLIC_URL}/img/placeholder2.jpg`}
+                text={'Обед, отдых, море'}
             />
             <Video 
                 videoOpacity = {videoOpacity}
                 videoUrl={`${process.env.PUBLIC_URL}/videos/example1.mp4`}
                 thumbnailVideoUrl={`${process.env.PUBLIC_URL}/img/placeholder3.jpg`}
+                text={'Поездки, жизнь лагеря'}
             />
             <Video 
                 videoOpacity = {videoOpacity}
                 videoUrl={`${process.env.PUBLIC_URL}/videos/example1.mp4`}
                 thumbnailVideoUrl={`${process.env.PUBLIC_URL}/img/placeholder4.jpg`}
+                text={'Наши педагоги'}
             />
 
         </div>

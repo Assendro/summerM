fca038f2ce4d8ed335a796b696485ea5c52b7567
Assendro <kirilll999127@gmail.com>
2025-02-20 10:46:42 +0300

    Добавили первый ролик

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -8,7 +8,7 @@ const Videos = ({videoOpacity}) => {
 
             <Video 
                 videoOpacity = {videoOpacity}
-                videoUrl={`${process.env.PUBLIC_URL}/videos/example1.mp4`}
+                videoUrl={`${process.env.PUBLIC_URL}/videos/LTO-1-1.mp4`}
                 thumbnailVideoUrl={`${process.env.PUBLIC_URL}/img/placeholder1.jpg`}
                 text={
                     <>

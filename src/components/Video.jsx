778e8a03e539134e6572579c806baf8f91b6647f
Assendro <kirilll999127@gmail.com>
2025-03-01 20:30:07 +0300

    Добавил к слайд шоу анимацию

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -32,6 +32,7 @@ const Video = ({ videoUrl, thumbnailVideoUrl, videoOpacity, text, placeholderOpa
           ) : (
             // Основное видео
             <ReactPlayer
+              className='react-player'
               width="100%"
               height='100%'
               url={videoUrl}

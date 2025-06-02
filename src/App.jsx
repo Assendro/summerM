4372343bc52d1e67f8a586de2edc57ed9293fdd6
Assendro <89154531+Assendro@users.noreply.github.com>
2025-06-02 13:46:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const videoSRC = [
     '',
-    'https://vkvideo.ru/video_ext.php?oid=1032371751&id=456239021&hash=3d35297f17006bbc&autoplay=1',
+    'https://vkvideo.ru/video_ext.php?oid=-230354258&id=456239022&hash=20f758e4518552a8&autoplay=1',
     'https://vkvideo.ru/video_ext.php?oid=1032371751&id=456239018&hd=2&hash=99a8f1618955d5ed&autoplay=1',
     'https://vkvideo.ru/video_ext.php?oid=1032371751&id=456239019&hd=2&hash=55bb73755994333f&autoplay=1',
     'https://vkvideo.ru/video_ext.php?oid=1032371751&id=456239023&hd=2&hash=c52a369c9e1932a6&autoplay=1'
